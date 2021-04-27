7d93deedce528e7bad389e3d6e0049c2fe06ffdd
Ketellin <54152021+Ketellin@users.noreply.github.com>
2021-04-27 15:45:05 -0300

    Requisições Usando ResponseStatus e RestController para um código mais limpo

--- src/main/java/br/edu/pucgoias/rest/controller/ClienteController.java
+++ src/main/java/br/edu/pucgoias/rest/controller/ClienteController.java
@@ -6,91 +6,67 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-/*
-O RequestMapping mapeia uma rota, através de um verbo http(RequestMethod.GET),
-e passa a tratar a requisição do cliente
+/**
+ * Refatorando para tornar o código restFull
  */
-@Controller
+//Assim como o @Controller, a @RestController também é usada como cotroller porem com mais funcionalidades
+//Ela já vem com a anotação @ResponseBody evitando o uso repetido.
+@RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
     private ClientesDao clientesDao;
 
-    /*
-    A injeção do DAO acontecerá pois o springboot perceberá a anotação Controller, perceberá também
-     o controlador recebendo parâmetros e
-    instaciará clientesDao, ou usará uma instancia já pronta do container
-     */
+
     public ClienteController(ClientesDao clientesDao) {
         this.clientesDao = clientesDao;
     }
 
-    //Por padrão o springboot trabalha com json
 
-    @GetMapping("/{id}") //Obter dados. Faz o mesmo que a anotação RequestMapping, porem com o GET definido. Veja o código.
-    @ResponseBody //Mapeará o objeto Cliente em um objeto Json. Se não for colocado, a requisição irá
-    //aguardar uma string que seria o endereço de uma página web. Como estamos trabalhando com REST, precisamos da anotação
-    public ResponseEntity getClienteById(@PathVariable Integer id){
-        //PathVariable define que o parâmetro id será recebido via requisição http, na url
-        //Se vc quiser usar o nome do parâmetro na url diferente do parâmetro no método, a anotação PathVariable
-        //deverá acompanhar o nome do parâmetro na url.
+    @GetMapping("/{id}")
+    public Cliente getClienteById(@PathVariable Integer id){
+        // Mais informações sobre Optional.orElseThrow
+        // https://medium.com/@racc.costa/optional-no-java-8-e-no-java-9-7c52c4b797f1
+        return clientesDao.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
 
-        /*
-        Esse metodo não foi implementado pois ele já existe no JpaRepository que foi extendido pela
-        classe ClientesDao
-        O retorno do método é um Optional pois pode ou não existir o cliente com o id especificado
-         */
-        Optional<Cliente> cliente = clientesDao.findById(id);
-        if(cliente.isPresent()) {
-            //Representa a resposta ao cliente
-            //ResponseEntity<Cliente> responseEntity = new ResponseEntity<>(cliente.get(), HttpStatus.OK);
-            //A LINHA ACIMA EQUIVALE A
-            return ResponseEntity.ok(cliente.get());
-            //o ok retornará o código 200. O método get retornará o cliente contido em Optional
-        }
-        return ResponseEntity.notFound().build();//retornará o código 440
     }
 
     @PostMapping
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Cliente cliente){
-        /*
-        Não confundir ResponseBody com RequestBody. Esse último é o que entra de dados, ou seja, o corpo da
-        Requisição.
-        O primeiro é o que será retornado, ou seja, Resposta.
-         */
-        Cliente cli = clientesDao.save(cliente);
-        return ResponseEntity.ok(cli);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save(@RequestBody Cliente cliente){
+
+        return clientesDao.save(cliente);
 
     }
 
     @DeleteMapping("{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Integer id){
-        Optional cli = clientesDao.findById(id);
-        if(cli.isPresent()) {
-            clientesDao.deleteById(id);
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        clientesDao.findById(id)
+                .map(cli -> {
+                    clientesDao.delete(cli);
+                    return cli;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
+
     }
 
-//    @RequestMapping(method = RequestMethod.PUT) ou
     @PutMapping("{id}")
-    @ResponseBody
-    public  ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Cliente cliente){
 
-        return clientesDao.findById(id).map(clienteExistente -> {
+        clientesDao.findById(id).map(clienteExistente -> {
             cliente.setId(clienteExistente.getId());
             clientesDao.save(cliente);
-            return  ResponseEntity.noContent().build();
-        }).orElseGet( () -> ResponseEntity.notFound().build());
+            return  clienteExistente;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
     /**
@@ -110,20 +86,14 @@ public class ClienteController {
      * @return ResponseEntity que representa um código de resposta ao cliente que fez a requisição
      */
     @GetMapping("")
-    public ResponseEntity find(Cliente filtro){
+    public List<Cliente> find(Cliente filtro){
         ExampleMatcher matcher = ExampleMatcher
                 .matching() //informando da combinação de atributos para filtrar
                 .withIgnoreCase() //Nos campos string, ignorar se está maiúsculo ou minúsculo
                 .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING); //Nos campos string consultar %filtro%
         //Example.of coleta apenas as propriedades preenchidas e cria o objeto ex
         Example ex = Example.of(filtro, matcher);
-        List<Cliente> lista = clientesDao.findAll(ex);
-        return ResponseEntity.ok(lista);
+        return clientesDao.findAll(ex);
     }
 
 }
-
-/*
-OBSERVAÇÃO:
-Conseguimos testar pelo browser por ser uma requisição GET que usa url.
- */
--- src/main/java/br/edu/pucgoias/rest/controller/ProdutoController.java
+++ src/main/java/br/edu/pucgoias/rest/controller/ProdutoController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.Optional;
 
-@Controller
+//Assim como o @Controller, a @RestController também é usada como cotroller porem com mais funcionalidades
+//Ela já vem com a anotação @ResponseBody evitando o uso repetido.
+@RestController
 @RequestMapping("/api/pedidos")
 public class ProdutoController {
 
@@ -21,7 +23,6 @@ public class ProdutoController {
     }
 
     @GetMapping("/{id}") //Obter os dados
-    @ResponseBody
     public ResponseEntity getProdutoById(@PathVariable Integer id){
 
         Optional<Produto> pr = produtoDao.findById(id);
